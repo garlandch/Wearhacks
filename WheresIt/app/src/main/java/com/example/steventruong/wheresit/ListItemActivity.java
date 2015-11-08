fa8e147bf3cd063029e4830ce3bde9fe6296d77c
David Zhang <dablosmo728@gmail.com>
2015-11-07 17:25:51 -0800

    new icons and action bar color

--- WheresIt/app/src/main/java/com/example/steventruong/wheresit/ListItemActivity.java
+++ WheresIt/app/src/main/java/com/example/steventruong/wheresit/ListItemActivity.java
@@ -1,8 +1,10 @@
 package com.example.steventruong.wheresit;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class ListItemActivity extends ActionBarActivity {
@@ -10,7 +12,7 @@ public class ListItemActivity extends ActionBarActivity {
     ListView listView;
 
     static final String[] ITEM =
-            new String[] { "Car" , "Keys"};
+            new String[] { "Bag" , "Bike", "Computer"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,23 @@ public class ListItemActivity extends ActionBarActivity {
         listView = (ListView) findViewById(R.id.list);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        myToolbar.setBackgroundColor(Color.rgb(255, 204, 0));
+        setTitle("Living Room");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         listView.setAdapter(new ItemArrayAdapter(this, ITEM));
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
