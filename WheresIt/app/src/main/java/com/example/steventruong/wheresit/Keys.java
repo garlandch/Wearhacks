67b78e9f224bd5fc00dc6b9518ab08fe9ce88b92
Izzy Benavente <ibenaven@usc.edu>
2015-11-07 20:01:58 -0800

    fixing merge conflict

---
+++ WheresIt/app/src/main/java/com/example/steventruong/wheresit/Keys.java
@@ -0,0 +1,12 @@
+package com.example.steventruong.wheresit;
+
+public class Keys {
+
+    public static final int KEY_CHOICE = 0;
+    public static final int CHOICE_VOICE = 0;
+    public static final int CHOICE_WAITING = 1;
+
+    public static final int KEY_RESULT = 1;
+    public static final int RESULT_ROOM1 = 0;
+    public static final int RESULT_ROOM2 = 1;
+}
--- WheresIt/app/src/main/java/com/example/steventruong/wheresit/MainActivity.java
+++ WheresIt/app/src/main/java/com/example/steventruong/wheresit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.steventruong.wheresit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,10 +11,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
+
+
     private Button mainBtn;
+
+    private static final UUID APP_UUID = UUID.fromString("af17efe7-2141-4eb2-b62a-19fc1b595595");
+    private PebbleKit.PebbleDataReceiver mDataReceiver;
+    private String mVoiceQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +60,33 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register to get updates from Pebble
+        if(mDataReceiver == null) {
+            mDataReceiver = new PebbleKit.PebbleDataReceiver(APP_UUID) {
+
+                @Override
+                public void receiveData(Context context, int transactionId, PebbleDictionary dict) {
+                    // Always ACK
+                    PebbleKit.sendAckToPebble(context, transactionId);
+
+                    // Did the user request a voice search?
+                    if( dict.getInteger(Keys.KEY_CHOICE) != null )
+                    {
+                        mVoiceQuery = dict.getString(Keys.KEY_RESULT);
+
+                        // TODO: Search for item and return location
+                    }
+                }
+
+            };
+            PebbleKit.registerReceivedDataHandler(getApplicationContext(), mDataReceiver);
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -58,12 +101,35 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_pebble) {
-            
+        if (id == R.id.action_pebble)
+        {
+            sideloadInstall(getApplicationContext(), "Where's_It.pbw");
         }
-
         return super.onOptionsItemSelected(item);
     }
+    /**
+     * Alternative sideloading method
+     * Source: http://forums.getpebble.com/discussion/comment/103733/#Comment_103733
+     */
+    public static void sideloadInstall(Context ctx, String assetFilename) {
+        try {
+            // Read .pbw from assets
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            File file = new File(ctx.getExternalFilesDir(null), assetFilename);
+            InputStream is = ctx.getResources().getAssets().open(assetFilename);
+            OutputStream os = new FileOutputStream(file);
+            byte[] pbw = new byte[is.available()];
+            is.read(pbw);
+            os.write(pbw);
+            is.close();
+            os.close();
 
-
+            // Install via Pebble Android app
+            intent.setDataAndType(Uri.fromFile(file), "application/pbw");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            ctx.startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(ctx, "App install failed: " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
