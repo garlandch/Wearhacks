1dae2b39911d32d363e799f67e66a4938c003a3e
David Zhang <dablosmo728@gmail.com>
2015-11-07 16:37:06 -0800

    action bar

--- WheresIt/app/src/main/java/com/example/steventruong/wheresit/ListItemActivity.java
+++ WheresIt/app/src/main/java/com/example/steventruong/wheresit/ListItemActivity.java
@@ -1,14 +1,13 @@
 package com.example.steventruong.wheresit;
 
-import com.example.steventruong.wheresit.ItemArrayAdapter;
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
-import android.widget.Toast;
-import android.view.View;
 
-public class ListItemActivity extends ListActivity {
+public class ListItemActivity extends ActionBarActivity {
+
+    ListView listView;
 
     static final String[] ITEM =
             new String[] { "Car" , "Keys"};
@@ -16,25 +15,13 @@ public class ListItemActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_activity);
 
-        setListAdapter(new ItemArrayAdapter(this, ITEM));
+        listView = (ListView) findViewById(R.id.list);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        listView.setAdapter(new ItemArrayAdapter(this, ITEM));
 
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-
-        //get selected items
-        String selectedValue = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
 }
